ec5e67dd1257ded6ac0a76bec60452f90e3fb3a1
Toriqul Islam Tonu <50527978+toriqultonu@users.noreply.github.com>
2022-11-11 20:01:31 +0600

    running initial controller

--- src/main/java/com/koshailimited/springbootdemoproject/controller/HelloController.java
+++ src/main/java/com/koshailimited/springbootdemoproject/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.koshailimited.springbootdemoproject.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @RequestMapping(value = "/hlw", method = RequestMethod.GET)
+    @GetMapping("/hlw ")
     public String helloWorld(){
         return "Welcome to spring boot";
     }
