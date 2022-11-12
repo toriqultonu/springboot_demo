4becdd2431a4dffe84bca3768235e8c97518095f
Toriqul Islam Tonu <50527978+toriqultonu@users.noreply.github.com>
2022-11-12 13:19:32 +0600

    logger added to all the controller functions

--- src/main/java/com/koshailimited/springbootdemoproject/controller/DepartmentController.java
+++ src/main/java/com/koshailimited/springbootdemoproject/controller/DepartmentController.java
@@ -2,6 +2,8 @@ package com.koshailimited.springbootdemoproject.controller;
 
 import com.koshailimited.springbootdemoproject.entity.Department;
 import com.koshailimited.springbootdemoproject.service.DepartmentService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,26 +15,31 @@ public class DepartmentController {
 
     @Autowired
     private DepartmentService departmentService;
+
+    private final Logger LOGGER = LoggerFactory.getLogger(DepartmentController.class);
     @PostMapping("/departments")
     public Department saveDepartment(@Valid @RequestBody Department department){
-
+         LOGGER.info("Inside saveDepartment of DepartmentController");
          departmentService.saveDepartment(department);
          return department;
     }
 
     @GetMapping("/getalldepartment")
     public List<Department> fetchDepartmentList(){
+        LOGGER.info("Inside fetchDepartmentList of DepartmentController");
         List<Department> departments = departmentService.getAllDepartment();
         return departments;
     }
 
     @GetMapping("/getdepartment/{id}")
     public Department fetchDepartmentById(@PathVariable("id") Long departmentId){
+        LOGGER.info("Inside fetchDepartmentById of DepartmentController");
         return departmentService.getDepartmentById(departmentId);
     }
 
     @DeleteMapping("/getdepartment/{id}")
     public String deleteDepartmentById(@PathVariable("id") Long departmentId){
+        LOGGER.info("Inside deleteDepartmentById of DepartmentController");
         departmentService.deleteDepartmentById(departmentId);
         return "Department deleted successfully";
     }
@@ -40,11 +47,13 @@ public class DepartmentController {
     @PutMapping("/getdepartment/{id}")
     public Department updateDepartment(@PathVariable("id") Long departmentId,
                                        @RequestBody Department department){
+        LOGGER.info("Inside updateDepartment of DepartmentController");
         return departmentService.updateDepartment(departmentId, department);
     }
 
     @GetMapping("getdepartment/name/{name}")
-    public Department getDepartmentById(@PathVariable("name") String departmentName){
+    public Department getDepartmentByName(@PathVariable("name") String departmentName){
+        LOGGER.info("Inside getDepartmentByName of DepartmentController");
         return departmentService.getDepartmentByName(departmentName);
     }
 }
