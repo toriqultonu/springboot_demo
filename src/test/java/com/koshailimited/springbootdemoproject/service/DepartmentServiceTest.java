fb345f077f881662913940b7c90c7b30b3dd8569
Toriqul Islam Tonu <50527978+toriqultonu@users.noreply.github.com>
2022-11-13 11:06:56 +0600

    first unit testing done with success

---
+++ src/test/java/com/koshailimited/springbootdemoproject/service/DepartmentServiceTest.java
@@ -0,0 +1,40 @@
+package com.koshailimited.springbootdemoproject.service;
+
+import com.koshailimited.springbootdemoproject.entity.Department;
+import com.koshailimited.springbootdemoproject.repository.DepartmentRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class DepartmentServiceTest {
+
+    @Autowired
+    private DepartmentService departmentService;
+
+    @MockBean
+    private DepartmentRepository departmentRepository;
+
+    @BeforeEach
+    void setUp() {
+
+        Department department = Department.builder().
+                                departmentId(1L).departmentAddress("RUET").departmentCode("03").departmentName("CSE")
+                                .build();
+
+        Mockito.when(departmentRepository.findByDepartmentNameIgnoreCase("CSE")).thenReturn(department);
+    }
+
+    @Test
+    public void whenValidDepartmentName_thenDepartmentShouldFound(){
+        String departmentName = "CSE";
+        Department found = departmentService.getDepartmentByName(departmentName);
+
+        assertEquals(departmentName, found.getDepartmentName());
+    }
+}
